27e436caba3812f02081b8907fa1433f3afb71a3
SILVANA MONSALVE OSPINA <silvanamonsalve265911@correo.itm.edu.co>
2022-02-27 20:50:30 -0500

    All classes are created

---
+++ SupermercadoPOO/SupermercadoPOO/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermercadoPOO
+{
+    public class Invoice : IPay
+    {
+        private Product _products;
+
+      
+
+        public Product AddProduct(Product _products)
+        {
+            return _products;
+
+        }
+        public decimal ValueToPay()
+        {
+
+            return ++_products.Price;
+        }
+
+        public override string ToString()
+        {
+            return $"{_products} " +
+                $"\n ============" +
+                $"TOTAL:       {ValueToPay()}\n";
+        }
+
+    }
+}
---
+++ SupermercadoPOO/SupermercadoPOO/VariablePriceProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace SupermercadoPOO
+{
+    public class VariablePriceProduct : Product
+    {
+        public string Measurement { get; set; }
+        public float Quantity { get; set; }
+
+        public override decimal ValueToPay()
+        {
+            return Price* (decimal)(Quantity*Tax);
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n Measurement......:{Measurement}" +
+                $"\n Quantity......: {Quantity}" +
+                $"\n Value.......:{ValueToPay():C2}";
+        }
+    }
+}
